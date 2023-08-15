3423410b8185d8f59887cf940270e1d1c45566f9
alissonsouza23 <alissonsouza2310@gmail.com>
2023-08-14 21:54:49 -0300

    feito a tela de cadastro de cliente

--- aula03/src/aula03/telaPrimcipal.java
+++ aula03/src/aula03/telaPrimcipal.java
@@ -28,13 +28,19 @@ public class telaPrimcipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         btnGravar = new javax.swing.JButton();
         textMensagem = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        textMensagem02 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        textMensagem03 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        textMensagem04 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        textMensagemOb = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setText("ola mundo !!");
 
         btnGravar.setText("gravar");
         btnGravar.addActionListener(new java.awt.event.ActionListener() {
@@ -43,32 +49,97 @@ public class telaPrimcipal extends javax.swing.JFrame {
             }
         });
 
+        jLabel2.setText("Nome completo");
+
+        textMensagem02.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textMensagem02ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("email");
+
+        jLabel3.setText("telefone");
+
+        textMensagem03.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textMensagem03ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("endereço");
+
+        textMensagem04.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textMensagem04ActionPerformed(evt);
+            }
+        });
+
+        textMensagemOb.setColumns(20);
+        textMensagemOb.setRows(5);
+        textMensagemOb.setText("Observações\n");
+        jScrollPane1.setViewportView(textMensagemOb);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnGravar))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(77, 77, 77)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 231, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(6, 6, 6)
+                                .addComponent(jLabel1)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(btnGravar))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(textMensagem)
+                                    .addComponent(jLabel2)
+                                    .addComponent(textMensagem02, javax.swing.GroupLayout.DEFAULT_SIZE, 265, Short.MAX_VALUE))
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addContainerGap())
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(textMensagem)))
-                .addContainerGap())
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(textMensagem03)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(6, 6, 6)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel3)))
+                            .addComponent(textMensagem04, javax.swing.GroupLayout.DEFAULT_SIZE, 265, Short.MAX_VALUE))
+                        .addGap(0, 0, Short.MAX_VALUE))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 192, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(45, 45, 45)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(textMensagem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 163, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel1)
+                .addGap(2, 2, 2)
+                .addComponent(textMensagem02, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(textMensagem03, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(textMensagem04, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
                 .addComponent(btnGravar)
                 .addContainerGap())
         );
@@ -80,9 +151,33 @@ public class telaPrimcipal extends javax.swing.JFrame {
        String X = textMensagem.getText(); 
        textMensagem.setText("");
        textMensagem.grabFocus(); 
+       
+       
+       textMensagem02.getText();
+        textMensagem02.setText("");
+        
+        textMensagem03.getText();
+        textMensagem03.setText("");
+        
+        textMensagem04.getText();
+        textMensagem04.setText(""); 
+        
+       
    
-       JOptionPane.showMessageDialog(null, X);
+       JOptionPane.showMessageDialog(null, "grabvado com sucesso");
     }//GEN-LAST:event_btnGravarActionPerformed
+
+    private void textMensagem02ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textMensagem02ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textMensagem02ActionPerformed
+
+    private void textMensagem03ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textMensagem03ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textMensagem03ActionPerformed
+
+    private void textMensagem04ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textMensagem04ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textMensagem04ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -122,6 +217,14 @@ public class telaPrimcipal extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnGravar;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField textMensagem;
+    private javax.swing.JTextField textMensagem02;
+    private javax.swing.JTextField textMensagem03;
+    private javax.swing.JTextField textMensagem04;
+    private javax.swing.JTextArea textMensagemOb;
     // End of variables declaration//GEN-END:variables
 }
